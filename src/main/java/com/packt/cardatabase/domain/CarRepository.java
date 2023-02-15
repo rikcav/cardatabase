ded31d1bf5859abee717dcfce9f5afa0b7ff1eed
jhenriquecavalcante <henriquearau000@gmail.com>
2023-02-15 07:44:52 -0300

    findBy update

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,16 +1,15 @@
 package com.packt.cardatabase.domain;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+@RepositoryRestResource
+public interface CarRepository extends CrudRepository<Car, Long> {
 
-public interface CarRepository extends CrudRepository <Car, Long> {
-	List<Car> findByBrand(String brand);
-	List<Car> findByColor(String color);
-	List<Car> findByYear(int year);
-	
-	List<Car> findByBrandAndModel(String brand, String model);
-	List<Car> findByBrandOrColor(String brand, String color);
-	
-	List<Car> findByBrandOrderByYearAsc(String brand);
+	List<Car> findByBrand(@Param("brand") String brand);
+
+	List<Car> findByColor(@Param("color") String color);
 }
